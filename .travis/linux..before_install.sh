3e16a27e9eb4063206b9c94efa2cbc732c6ab972
liushuyu <liushuyu011@gmail.com>
2016-05-11 11:16:08 +0800

    Travis new infrastructure test

--- .travis/linux..before_install.sh
+++ .travis/linux..before_install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:kalakris/cmake -y;
-sudo add-apt-repository ppa:andrewrk/libgroove -y;
-if [ $QT5 ]; then
-	sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
-fi
-sudo apt-get update -qq
+#sudo add-apt-repository ppa:kalakris/cmake -y;
+#sudo add-apt-repository ppa:andrewrk/libgroove -y;
+#if [ $QT5 ]; then
+#	sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
+#fi
+#sudo apt-get update -qq
--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-PACKAGES="cmake libsndfile-dev fftw3-dev libvorbis-dev  libogg-dev
-	libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev libstk0-dev
-	libfluidsynth-dev portaudio19-dev wine-dev g++-multilib libfltk1.3-dev
-	libgig-dev libsoundio-dev"
+#PACKAGES="cmake libsndfile-dev fftw3-dev libvorbis-dev  libogg-dev
+#	libasound2-dev libjack-dev libsdl-dev libsamplerate0-dev libstk0-dev
+#	libfluidsynth-dev portaudio19-dev wine-dev g++-multilib libfltk1.3-dev
+#	libgig-dev libsoundio-dev"
 
-if [ $QT5 ]; then
-	PACKAGES="$PACKAGES qtbase5-dev qttools5-dev-tools qttools5-dev"
-else
-	PACKAGES="$PACKAGES libqt4-dev"
-fi
+#if [ $QT5 ]; then
+#	PACKAGES="$PACKAGES qtbase5-dev qttools5-dev-tools qttools5-dev"
+#else
+#	PACKAGES="$PACKAGES libqt4-dev"
+#fi
 
-sudo apt-get install -y $PACKAGES
+#sudo apt-get install -y $PACKAGES
